f2e36e7d7b79fd163168ff44af201eeadc6dea52
alovoce <kleber@alovoce.com.br>
2025-08-05 22:05:18 -0300

    Delete agenda directory

--- agenda/conexao.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$servidor = "localhost";
-	$usuario = "cpf_root";
-	$senha = "Kleber33@";
-	$dbname = "pdv";
-	
-	//Criar a conexao
-	$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
-	
