09e57e1e0e7802ae6385ba93d151b033f05f1c11
alovoce <kleber@alovoce.com.br>
2025-08-05 21:47:35 -0300

    Add files via upload

---
+++ app/agenda/conexao.php
@@ -0,0 +1,9 @@
+<?php
+	$servidor = "localhost";
+	$usuario = "cpf_root";
+	$senha = "Kleber33@";
+	$dbname = "pdv";
+	
+	//Criar a conexao
+	$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+	
--- app/index.php
+++ app/index.php
@@ -1,82 +1,144 @@
 <?php
-$db = new PDO('mysql:host=localhost', 'root', null);
+date_default_timezone_set('America/Sao_Paulo');
+ini_set( 'display_errors', 0 );//oculta  erros
 
-function getOSInformation()
- {
-     if (false == function_exists("shell_exec") || false == is_readable("/etc/os-release")) {
-         return null;
-     }
+session_start();
 
-      $os         = shell_exec('cat /etc/os-release');
-     $listIds    = preg_match_all('/.*=/', $os, $matchListIds);
-     $listIds    = $matchListIds[0];
+  $servidor = "localhost";
+  $usuario = "cpf_root";
+  $senha = "Kleber33@";
+  $dbname = "cpf_pdv";
 
-      $listVal    = preg_match_all('/=.*/', $os, $matchListVal);
-     $listVal    = $matchListVal[0];
 
-      array_walk($listIds, function(&$v, $k){
-         $v = strtolower(str_replace('=', '', $v));
-     });
+  $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
 
-      array_walk($listVal, function(&$v, $k){
-         $v = preg_replace('/=|"/', '', $v);
-     });
+$select_table = "SELECT * FROM cor WHERE id LIKE '1'";
+$verifica_tabela = mysqli_query($conn, $select_table);
+$verifica_tabela = mysqli_fetch_assoc($verifica_tabela);
+$cor = $verifica_tabela['cor'];
+$_SESSION['cor'] = $cor;
 
-      return array_combine($listIds, $listVal);
- }
-$osInfo = getOSInformation();
-?>
-<!doctype html>
-<html lang=en>
+
+
+
+
+if($_SESSION['login'] == 0){?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
 <head>
-    <meta charset=utf-8>
-    <title>Hello World from Docker-LAMP</title>
 
-    <style>
-        @import 'https://fonts.googleapis.com/css?family=Montserrat|Raleway|Source+Code+Pro';
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
 
-        body { font-family: 'Raleway', sans-serif; }
-        h2 { font-family: 'Montserrat', sans-serif; }
-        pre {
-            font-family: 'Source Code Pro', monospace;
+  <title>DataFood</title>
 
-            padding: 16px;
-            overflow: auto;
-            font-size: 85%;
-            line-height: 1.45;
-            background-color: #f7f7f7;
-            border-radius: 3px;
+  <link href="mvc/common/css/animate.min.css" rel="stylesheet"/><!--ESTE COMANDO CRIA A NOTIFICAÃO ANIMADA  -->
+  <link href="mvc/common/css/bootstrap-datepicker.css" rel="stylesheet"/>
 
-            word-wrap: normal;
-        }
+  <link rel="shortcut icon"  href="mvc/common/img/beer.png"><!--este comando muda o icone da janela-->
 
-        .container {
-            max-width: 1024px;
-            width: 100%;
-            margin: 0 auto;
-        }
-    </style>
+  <!-- Custom fonts for this template-->
+  <link href="mvc/common/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  
+
+
+  <!-- Custom styles for this template-->
+  <link href="mvc/common/css/sb-admin-2.min.css" rel="stylesheet">
+  
 </head>
-<body>
-    <div class="container">
-        <header>
-            <img src="https://cdn.rawgit.com/mattrayner/docker-lamp/831976c022782e592b7e2758464b2a9efe3da042/docs/logo.svg" alt="Docker LAMP logo" />
-            <h2>Welcome to <a href="https://github.com/mattrayner/docker-lamp" target="_blank">Docker-Lamp</a> a.k.a mattrayner/lamp</h2>
-        </header>
-        <article>
-            <p>
-                For documentation, <a href="https://github.com/mattrayner/docker-lamp" target="_blank">click here</a>.
-            </p>
-        </article>
-        <section>
-            <pre>
-OS: <?php echo $osInfo['pretty_name']; ?><br/>
-Apache: <?php echo apache_get_version(); ?><br/>
-MySQL Version: <?php echo $db->getAttribute( PDO::ATTR_SERVER_VERSION ); ?><br/>
-PHP Version: <?php echo phpversion(); ?><br/>
-phpMyAdmin Version: <?php echo getenv('PHPMYADMIN_VERSION'); ?>
-            </pre>
-        </section>
+
+<body class="bg-gradient-primary">
+
+  <div class="container">
+
+    <!-- Outer Row -->
+    <div class="row justify-content-center">
+
+      <div class="col-xl-6 col-lg-12 col-md-9">
+
+        <div class="card o-hidden border-0 shadow-lg my-5">
+          <div class="card-body p-0">
+            <!-- Nested Row within Card Body -->
+            <div class="row">
+              
+              <div class="col-lg-12">
+                <div class="p-5">
+                  <div class="text-center">
+                    <h1 style="color: red;">Sistema DataFood</h1>
+                    <h1 class="h4 text-gray-900 mb-4">Bem-Vindo!</h1>
+                    <h6 style="padding: 15px;">Digite sua senha e seu login para ter acesso ao sistema !</h6>
+                  </div>
+                  <form method="POST" action="mvc/model/login.php">
+                    <div class="form-group">
+                      <input  class="form-control form-control-user text-center" name="login" id="login" aria-describedby="emailHelp" placeholder="Digte seu login...">
+                    </div>
+                    <div class="form-group">
+                      <input type="password" class="form-control form-control-user text-center" name="senha" id="senha" placeholder="Senha">
+                    </div>
+                    <div class="form-group">
+                      <div class="custom-control custom-checkbox small">
+                        <input type="checkbox" class="custom-control-input" id="customCheck">
+                        
+                      </div>
+                    </div>
+					<div class="col-12 text-center" id="mensagem" style="visibility: visible"><?php if (isset($_SESSION['msg'])) {echo $_SESSION['msg'];  unset($_SESSION['msg']); }?></div>
+
+                    <button style="color: white;" type="submit" class="btn btn-primary btn-user btn-block">
+                      Login
+                    </button>
+                    <hr>
+                    
+                  </form>
+                  <hr>
+                  <div class="text-center">
+                    <a class="small" href="recuperar_senha.php">Esquceu a Senha?</a>
+                  </div>
+                  <div class="text-center">
+                    <a class="small" href="criar_senha.php">Cadastrar Administrador</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
     </div>
+
+  </div>
+
+
+
 </body>
+
 </html>
+
+
+
+
+
+
+<?php }elseif($_SESSION['login'] == 1){
+	include('mvc/classes/system.class.php');
+
+	if (isset($_GET['view'])) {
+		$view = $_GET['view'];
+	}
+	else{
+
+		$view = 'Dashboard1';
+		//carrega uma pagina de erro ou em branco
+	}
+
+
+	$system = new System($view);
+	}
+
+
+ ?>
